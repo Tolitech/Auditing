d7d3a8e2a781df06c34e5f7c5fcf116919cace34
Eduardo Tolino <eduardo.tolino@tolitech.com.br>
2020-06-27 09:50:24 -0300

    Alpha 7

--- Auditing/Audit.cs
+++ Auditing/Audit.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Reflection;
 using Tolitech.CodeGenerator.Auditing.Models;
 
 namespace Tolitech.CodeGenerator.Auditing
 {
     public class Audit : IAudit
     {
+        private static IList<string> _availableEntities = new List<string>();
+        private static IList<string> _entitiesEnabled = new List<string>();
+
         public Audit()
         {
             Items = new ConcurrentQueue<AuditModel>();
@@ -42,5 +47,40 @@ namespace Tolitech.CodeGenerator.Auditing
         }
 
         public ConcurrentQueue<AuditModel> Items { get; }
+
+        public static void AddAssembly(string assemblyString)
+        {
+            var assembly = AppDomain.CurrentDomain.Load(assemblyString);
+            AddAssembly(assembly);
+        }
+
+        public static void AddAssembly(Assembly assembly)
+        {
+            foreach (Type t in assembly.GetTypes())
+            {
+                if (t.IsSubclassOf(typeof(AuditableEntity)))
+                    _availableEntities.Add(assembly.FullName);
+            }
+        }
+
+        public static void EnableEntitity(string fullName)
+        {
+            _entitiesEnabled.Add(fullName);
+        }
+
+        public static void ClearEntities()
+        {
+            _entitiesEnabled.Clear();
+        }
+
+        public static bool IsEntityEnabled(string fullName)
+        {
+            return _availableEntities.Contains(fullName);
+        }
+
+        public static IList<string> GetAvailableEntities()
+        {
+            return _availableEntities;
+        }
     }
 }
